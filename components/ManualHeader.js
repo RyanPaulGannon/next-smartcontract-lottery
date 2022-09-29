d181120a23d3c151b97fe376642ec9be8f33a821
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-09-29 16:29:40 +0100

    Added more functionality to 'connect' button

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -1,8 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useMoralis } from "react-moralis"
 
 export default function ManualHeader() {
-  const { enableWeb3, account } = useMoralis()
+  const { enableWeb3, account, isWeb3Enabled, isWeb3EnableLoading, Moralis, deactivateWeb3 } =
+    useMoralis()
+
+  useEffect(() => {
+    if (isWeb3Enabled) return
+    if (typeof window !== "undefined") {
+      if (window.localStorage.getItem("Connected")) {
+        //enableWeb3()
+      }
+    }
+  }, [isWeb3Enabled])
+
+  useEffect(() => {
+    Moralis.onAccountChanged((account) => {
+      console.log(`Account changed to ${account}`)
+      if (account == null) {
+        window.localStorage.removeItem("Connected")
+        deactivateWeb3()
+        console.log(`Null account found`)
+      }
+    })
+  }, [])
 
   return (
     <div>
@@ -11,7 +32,17 @@ export default function ManualHeader() {
           Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}
         </div>
       ) : (
-        <button onClick={async () => await enableWeb3()}>Connect</button>
+        <button
+          onClick={async () => {
+            await enableWeb3()
+            if (typeof window !== "undefined") {
+              window.localStorage.setItem("Connected", "Injected")
+            }
+          }}
+          disabled={isWeb3EnableLoading}
+        >
+          Connect
+        </button>
       )}
     </div>
   )
